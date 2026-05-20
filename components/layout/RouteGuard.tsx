2c01638a98146e861df0e263309323c8e578e13a
reyner23 <reyraynaldi@prieds.com>
2026-05-20 20:24:04 +0700

    Fix typescript error in RouteGuard

--- components/layout/RouteGuard.tsx
+++ components/layout/RouteGuard.tsx
@@ -1,6 +1,13 @@
 "use client";
 
-export const RouteGuard = ({ children }: { children: React.ReactNode }) => {
+// Kita tambahkan 'requiredPage?: string' agar TypeScript tahu ini legal
+export const RouteGuard = ({ 
+  children, 
+  requiredPage 
+}: { 
+  children: React.ReactNode;
+  requiredPage?: string; 
+}) => {
   // Bypass sementara agar tidak error saat testing UI
   return <>{children}</>;
 };
